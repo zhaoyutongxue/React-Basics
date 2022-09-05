247054bc5ab393d3b5d45cd631e8bfea81607d3a
Zhaoyu(Henry) Zhong <zhaoyuzhong2015@gmail.com>
2022-09-05 16:25:10 +1000

    Update Home.js

--- myblog/src/Home.js
+++ myblog/src/Home.js
@@ -2,26 +2,7 @@ import { useState, useEffect } from "react";
 import ProjectList from "./ProjectList";
 
 const Home = () => {
-  const [projects, setProjects] = useState([
-    {
-      title: "Pretty Good Restaurants",
-      body: "this is the website where friends can share their favourite restaurants",
-      author: "Henry",
-      id: 1,
-    },
-    {
-      title: "auto tester",
-      body: "this is the tester that can test devices efficiently",
-      author: "Elsafe",
-      id: 2,
-    },
-    {
-      title: "another react project",
-      body: "this is the project that uses TypeScript and React JS",
-      author: "Henry React",
-      id: 3,
-    },
-  ]);
+  const [projects, setProjects] = useState(null);
   const handleDelete = (id) => {
     const newProjects = projects.filter((project) => project.id !== id);
     setProjects(newProjects);
