694b9853adbda0fcf3a0ff1daeeac9d407b1f346
Zhaoyu(Henry) Zhong <zhaoyuzhong2015@gmail.com>
2022-09-05 21:11:31 +1000

    add error handling

--- myblog/src/Home.js
+++ myblog/src/Home.js
@@ -3,19 +3,36 @@ import ProjectList from "./ProjectList";
 
 const Home = () => {
   const [projects, setProjects] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:8080/projects")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data for that resource");
+        }
         const result = res.json();
         return result;
       })
       .then((data) => {
+        setError(false);
         setProjects(data);
+        setIsPending(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
       });
   }, []);
 
-  return <div>{projects && <ProjectList projects={projects} />}</div>;
+  return (
+    <div>
+      {error && <div>{error}</div>}
+      {isPending && <div>is loading...</div>}
+      {projects && <ProjectList projects={projects} />}
+    </div>
+  );
 };
 
 export default Home;
